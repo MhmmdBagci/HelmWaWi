b3feb36e99d8efb849e092dfb14ec42aa02caf2d
MhmmdBagci <Muhammed.Bagci@skyered-education.de>
2025-02-05 14:39:09 +0100

    update code

---
+++ HelmWaWi/main.py
@@ -0,0 +1,16 @@
+#from HelmWaWi.Controller import Logic
+#from HelmWaWi.View import View
+from User.View import userView
+
+"""
+Dieses Script ist der Entrypoint. Es startet alles Methoden nach und nach
+um ein Ablauf zu gewährleisten
+"""
+
+#Logic.generateStrings()
+#View.getInput()
+#Logic.getHelmets()
+#View.printHelmet()
+
+userView.getUserData()
+
